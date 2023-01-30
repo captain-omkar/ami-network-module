0b0f5d29698b2ad62b3c4cda98c52dfab3b5d719
captain-omkar <112746387+captain-omkar@users.noreply.github.com>
2023-01-30 10:53:14 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,83 +1,35 @@
-
-
 variable "vpc_cidr" {
-	
 }
+
+variable "vpc_name" {
+}
+
+variable "IG_name" {
+}
+
 
 
 variable "private_subnets_1" {
-   type = map
-   default = {
-      sub-1 = {
-         name = "ami-network-tgw-subnet-1"
-         cidr = "10.96.7.0/27"
-      }
-      sub-2 = {
-         name = "ami-network-trust-subnet-1"
-         cidr = "10.96.2.0/27"
-      }
-
-      sub-3 = {
-        name = "ami-network-app-subnet-1"
-        cidr = "10.96.6.0/25"
-        }
-      
-   }
+   type = map(map(string))
 }
 
 variable "private_subnets_2" {
-   type = map
-   default = {
-      sub-1 = {
-         name = "ami-network-tgw-subnet-2"
-         cidr = "10.96.7.32/27"
-      }
-      sub-2 = {
-         name = "ami-network-trust-subnet-2"
-         cidr = "10.96.2.32/27"
-      }
-
-      sub-3 = {
-        name = "ami-network-app-subnet-2"
-        cidr = "10.96.6.128/25"
-        }
-      
-   }
+   type = map(map(string))
 }
 
 variable "public_subnets" {
-   type = map
-   default = {
-      sub-1 = {
-         name = "ami-network-lb-subnet-1"
-         cidr = "10.96.4.0/24"
-      }
-      sub-2 = {
-         name = "ami-network-lb-subnet-2"
-         cidr = "10.96.5.0/24"
-      }
-
-      sub-3 = {
-        name = "ami-network-untrust-subnet-1"
-        cidr = "10.96.0.0/24"
-        }
-
-      sub-4 = {
-         name = "ami-network-untrust-subnet-2"
-         cidr = "10.96.1.0/24"
-      }
-      sub-5 = {
-         name = "ami-network-management-subnet-1	 "
-         cidr = "10.96.3.0/28"
-      }
-
-      sub-6 = {
-        name = "ami-network-management-subnet-2	"
-        cidr = "10.96.3.16/28"
-        }  
-      
-   }
+   type = map(map(string))
+   
 }
+
+variable "public_subnet_nat1" {
+   type = map(string)
+}
+
+variable "public_subnet_nat2" {
+   type = map(string)
+}
+
 
 
 
